344058299786cb300dcb65c8392a0f43cf600b78
Benjamin Guillet <benjamin.guillet@gmail.com>
2013-04-03 23:33:08 -0500

    randomly select 2 friends and display them, profile pic aren't updated though

--- src/com/scopely/sack/SelectionFragment.java
+++ src/com/scopely/sack/SelectionFragment.java
@@ -1,11 +1,15 @@
 package com.scopely.sack;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,16 +26,21 @@ import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphObject;
-import com.facebook.model.GraphUser;
 import com.facebook.widget.ProfilePictureView;
 
 public class SelectionFragment extends Fragment implements Constants {
-	private ProfilePictureView profilePictureView;
-	private TextView userNameView;
 	private static final int REAUTH_ACTIVITY_CODE = 100;
 	private static final String TAG = "com.scopely.sack";
 	
+	private ProfilePictureView profilePictureFriend1;
+	private ProfilePictureView profilePictureFriend2;
+	private TextView userNameFriend1;
+	private TextView userNameFriend2;
+	
 	private FriendsData friends;
+	private int[] randomFriends;
+	private ArrayList<Integer> tabuListFriendsId;
+	private int friendCount;
 	
 	private UiLifecycleHelper uiHelper;
 	private Session.StatusCallback callback = new Session.StatusCallback() {
@@ -47,6 +56,9 @@ public class SelectionFragment extends Fragment implements Constants {
 	    uiHelper = new UiLifecycleHelper(getActivity(), callback);
 	    uiHelper.onCreate(savedInstanceState);
 	    friends = new FriendsData(getActivity());
+	    tabuListFriendsId = new ArrayList<Integer>();
+	    randomFriends = new int[2];
+	    friendCount = 0;
 	}
 	
 	@Override
@@ -85,43 +97,24 @@ public class SelectionFragment extends Fragment implements Constants {
 	    super.onCreateView(inflater, container, savedInstanceState);
 	    View view = inflater.inflate(R.layout.selection, container, false);
 	    
-	    profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
-	    profilePictureView.setCropped(true);
-	    userNameView = (TextView) view.findViewById(R.id.selection_user_name);
+	    profilePictureFriend1 = (ProfilePictureView) view.findViewById(R.id.profile_pic_friend_1);
+	    profilePictureFriend2 = (ProfilePictureView) view.findViewById(R.id.profile_pic_friend_2);
+	    profilePictureFriend1.setCropped(true);
+	    profilePictureFriend2.setCropped(true);
+	    
+	    userNameFriend1 = (TextView) view.findViewById(R.id.user_name_friend_1);
+	    userNameFriend2 = (TextView) view.findViewById(R.id.user_name_friend_2);
+	   
 	    
 	    // Check for an open session
 	    Session session = Session.getActiveSession();
 	    if (session != null && session.isOpened()) {
-	        // Get the user's data
-	        makeMeRequest(session);
+	        // Get the user's friends
 	        makeFriendsRequest(session);
 	    }
-	   
+        
+	    pickTwoRandomFriends();
 	    return view;
-	}
-	
-	private void makeMeRequest(final Session session) {
-	    // Make an API call to get user data and define a 
-	    // new callback to handle the response.
-	    Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
-	        @Override
-	        public void onCompleted(GraphUser user, Response response) {
-	            // If the response is successful
-	            if (session == Session.getActiveSession()) {
-	                if (user != null) {
-	                    // Set the id for the ProfilePictureView
-	                    // view that in turn displays the profile picture.
-	                    profilePictureView.setProfileId(user.getId());
-	                    // Set the Textview's text to the user's name.
-	                    userNameView.setText(user.getName());
-	                }
-	            }
-	            if (response.getError() != null) {
-	                // TODO:
-	            }
-	        }
-	    });
-	    request.executeAsync();
 	}
 	
 	private void makeFriendsRequest(final Session session) {
@@ -135,8 +128,9 @@ public class SelectionFragment extends Fragment implements Constants {
             HttpMethod.GET,                 
             new Request.Callback() {       
                 public void onCompleted(Response response) {
-                    Log.i(TAG, "Result: " + response.toString());
-                    try {
+                    //Log.d(TAG, "Result: " + response.toString());
+                    // TODO: refactor for taking care of the json somewhere else
+                	try {
                     	GraphObject graphObject = response.getGraphObject();
                         if (graphObject != null) {
                             JSONObject jsonObject = graphObject.getInnerJSONObject();
@@ -145,7 +139,7 @@ public class SelectionFragment extends Fragment implements Constants {
                             	for (int i = 0; i < friends.length(); ++i) {
                             		JSONObject friend = (JSONObject) friends.get(i);
                             		// the uid can overflows a 32 bits integer
-                            		long uid          = Long.parseLong(friend.get("uid").toString());
+                            		long uid         = Long.parseLong(friend.get("uid").toString());
                             		String firstName = friend.get("first_name").toString();
                             		String lastName  = friend.get("last_name").toString();
                             		addFriend(uid, firstName, lastName);	
@@ -170,8 +164,6 @@ public class SelectionFragment extends Fragment implements Constants {
 	
 	private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
 	    if (session != null && session.isOpened()) {
-	        // Get the user's data.
-	        makeMeRequest(session);
 	        makeFriendsRequest(session);
 	    }
 	}
@@ -186,5 +178,60 @@ public class SelectionFragment extends Fragment implements Constants {
 		values.put(CREATED_AT, unixTime);
 		values.put(UPDATED_AT, unixTime);
 		db.insertOrThrow(TABLE_NAME, null, values);
+	}
+	
+	private void pickTwoRandomFriends() {
+		if (friendCount == 0) {
+			SQLiteDatabase db = friends.getReadableDatabase();
+			Cursor fCount = db.rawQuery("SELECT COUNT(*) FROM "+ TABLE_NAME +";", null);
+			fCount.moveToFirst();
+			friendCount = fCount.getInt(0);
+			fCount.close();
+		}
+		
+		Random randomGenerator = new Random();
+		int counter = 0;
+		while(counter < 2) {
+			Integer randomFriend = Integer.valueOf(randomGenerator.nextInt(friendCount));
+			if (!tabuListFriendsId.contains(randomFriend)) {
+				randomFriends[counter++] = randomFriend;	
+			}
+		}
+		//Log.d(TAG, "friendCount: " + friendCount);
+		//Log.d(TAG, "friend1: " + randomFriends[0]);
+		//Log.d(TAG, "friend2: " + randomFriends[1]);
+		
+		displayFriends(randomFriends[0], randomFriends[1]);
+	}
+
+	private void displayFriends(int friend1, int friend2) {
+		// TODO: prevent recall when screen orientation has changed:
+		// http://stackoverflow.com/questions/456211/activity-restart-on-rotation-android
+		SQLiteDatabase db = friends.getReadableDatabase();
+		
+		String[] FRIEND = {FACEBOOK_ID, FIRST_NAME, LAST_NAME};
+		String WHERE    =  _ID + " = " + friend1 + " OR " + _ID + " = " + friend2;
+		Cursor cursor = db.query(TABLE_NAME, FRIEND, WHERE, null, null, null, null);
+		
+		long[] friendsFacebookID = new long[2];
+		String[] friendsName     = new String[2];
+		int i = 0;
+		while (cursor.moveToNext()) {
+			friendsFacebookID[i] = cursor.getLong(0);
+			friendsName[i] = cursor.getString(1) + " " + cursor.getString(2);
+			++i;
+		}
+		cursor.close();
+		
+		for (int j = 0; j < 2; ++j) {
+			Log.d(TAG, "id" + j + ": " + friendsFacebookID[j]);
+			Log.d(TAG, "name" + j + ": " + friendsName[j]);
+		}
+		
+		profilePictureFriend1.setProfileId(Long.toString(friendsFacebookID[0]));
+		profilePictureFriend2.setProfileId(Long.toString(friendsFacebookID[1]));
+		
+		userNameFriend1.setText(friendsName[0]);
+	    userNameFriend2.setText(friendsName[1]);
 	}
 }
